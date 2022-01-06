bebb05794574b0c6356db4476f39b56fb4b1871f
Capcomfag <capcomfag@gmail.com>
2022-01-06 00:14:09 -0300

    3 files modified

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,4 +1,4 @@
-import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 export class AppRoutingModule {}
